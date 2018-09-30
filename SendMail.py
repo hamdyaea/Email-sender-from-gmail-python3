1a3fc4923e0eef5db9a5d3da21d172ef9e3f9fe1
hamdyaea <hamdy.aea@gmail.com>
2018-09-30 14:17:41 +0200

    First commit

---
+++ SendMail.py
@@ -0,0 +1,23 @@
+# Developer : Hamdy Abou El Anein
+
+import smtplib
+TO = 'anywhere@mail.com'
+SUBJECT = 'Text subject of the mail'
+TEXT = 'Text of the mail'
+
+gmail_sender = 'yourMail@gmail.com'
+gmail_passwd = 'password'
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmail_sender, gmail_passwd)
+BODY = '\r\n'.join(['To: %s' % TO,
+                    'From: %s' % gmail_sender,
+                    'Subject: %s' % SUBJECT,
+                    '', TEXT])
+try:
+    server.sendmail(gmail_sender, [TO], BODY)
+    print ('email sent')
+except:
+    print ('error sending mail')
+server.quit()
