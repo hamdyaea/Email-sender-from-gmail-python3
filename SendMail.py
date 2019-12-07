442fa96ff15c247da65ac472039fc403c5ac62bc
hamdyaea <hamdy.aea@protonmail.com>
2019-12-07 18:06:20 +0100

    Class and code beauty

--- SendMail.py
+++ SendMail.py
@@ -1,19 +1,31 @@
+#!/usr/bin/env python3
+
 # Developer : Hamdy Abou El Anein
+# hamdy.aea@protonmail.com
+
 
 import smtplib
 
-fromaddr = 'fromuser@gmail.com'
-toaddrs  = 'touser@gmail.com'
-msg = 'There was a terrible error that occured and I wanted you to know!'
+
+class Mailer:
+    def __init__(self):
+        self.fromaddr
+        self.toaddrs
+        self.msg
+
+
+Mailer.fromaddr = "fromuser@gmail.com"
+Mailer.toaddrs = "touser@gmail.com"
+Mailer.msg = "There was a terrible error that occured and I wanted you to know!"
 
 
 # Credentials (if needed)
-username = 'username'
-password = 'password'
+username = "username"
+password = "password"
 
 # The actual mail send
-server = smtplib.SMTP('smtp.gmail.com:587')
+server = smtplib.SMTP("smtp.gmail.com:587")
 server.starttls()
-server.login(username,password)
-server.sendmail(fromaddr, toaddrs, msg)
+server.login(username, password)
+server.sendmail(Mailer.fromaddr, Mailer.toaddrs, Mailer.msg)
 server.quit()
